14ef71b0a39359f7da09f138e2bdf27edce78fcf
Nabil Ouerhani <n.ouerhani@gmail.com>
2019-09-12 11:51:38 +0200

    Creation

---
+++ GC/src/MyGC.java
@@ -0,0 +1,16 @@
+
+public class MyGC {
+	
+	static int [] n;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		while (true) {
+			
+			n = new int [100000000];
+		}
+
+	}
+
+}
